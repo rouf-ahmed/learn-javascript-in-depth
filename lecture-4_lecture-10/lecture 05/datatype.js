d0e5fbd950dd80721770db5e4a83a6aa658f6a11
Abdur Rouf Ahmed <abdurroufahmed83@gmail.com>
2025-02-07 14:44:45 +0000

    initail commit

--- lecture-4_lecture-10/lecture 05/datatype.js
+++ lecture-4_lecture-10/lecture 05/datatype.js
@@ -20,18 +20,27 @@ let state;
 
 //NOTES:
 
-// number => 2 to power 53 // example let age = 18
-// bigint
-// string = " " //in double "
-// boolean => true/false (1&0) // example let isLoggedIn = false/true
-// null => standalone value 
-// undefined => it means value is not assigned till now... //example of undefined is let state;(value is not defined so it will be undefined)
+// number => 2 to power 53 // example let age = 18;
+// bigint=> let big = 12345678987n; //for very large number
+// string = " " // text includes in quotes "
+// boolean => true/false // example let isLoggedIn = false/true
+// null => standalone value  // intentional absence of value
+// undefined => it means value is not assigned till now (declare but not assign)... //example of undefined is let state;(value is not defined so it will be undefined)
+
 // symbol => uniqueness(mostly used in react)
 
 
-//Object 
+//Object ..
+
+
 console.log(typeof "Rouf");                 //typeof-string
 console.log(typeof age);                    //typeof-number
 
-//If age is not defined like this; age; then output will be undefined.. if i will console.log(typeof age)..
-console.log(typeof null);                   // nulltypeof-object
+
+console.log(typeof null);                   // nulltypeof-object (due to a historical js bug)
+// but noted >>>> null is not an object its a primitive data type in js....its represnts 'no value' or 'empty value' not an object......
+
+// ........................................................................................
+
+//If age is not defined like this; age; then output will be undefined.. if i will console.log(typeof age);....
+console.log(typeof umdefined);              //undefined type --undefined
